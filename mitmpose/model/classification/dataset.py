0d665af782c6d511c4deb00777b64230f9d3dda5
safoex <safoex@hotmail.com>
2020-09-14 17:56:41 +0200

    added worker

--- mitmpose/model/classification/dataset.py
+++ mitmpose/model/classification/dataset.py
@@ -3,9 +3,25 @@ import torch
 from torch.utils.data import Dataset
 from mitmpose.model.so3.grids import Grid
 from mitmpose.model.so3 import ObjectRenderer
+from mitmpose.model.so3.dataset import RenderedDataset
+from mitmpose.model.so3.augment import AugmentedDataset
 from tqdm import tqdm
 import os
 
 
 class ManyObjectRenderedDataset(Dataset):
-    def __init__(self, grider: Grid, models: dict, res=128, camera_dist=0.5, render_res=640):
+    def __init__(self, grider: Grid, models: dict, DatasetClass=AugmentedDataset, res=128, camera_dist=0.5, render_res=640):
+        self.models = models
+        self.datasets = None
+        self.default_params = {
+            'camera_dist' : camera_dist,
+            'render_res' : render_res,
+            'res' : res
+        }
+        self.DatasetClass = DatasetClass
+
+    def create_dataset(self, folder):
+        self.datasets = {}
+        for model_name, params in self.models.items():
+            mparams = self.default_params.copy()
+            mparams.update(params)
--- mitmpose/model/so3/ae.py
+++ mitmpose/model/so3/ae.py
@@ -110,7 +110,7 @@ class AEDataModule(pl.LightningDataModule):
         self.dataset.load_dataset(self.dataset_folder)
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True, num_workers=4)
     #
     # def val_dataloader(self) -> DataLoader:
     #     return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=True)
