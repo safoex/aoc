found a correct rotation

mitmpose/routines/test/inference/analyze.py
@@ -4,10 +4,10 @@ import pickle
 import os
 
 workdir = '/home/safoex/Documents/data/aae/release2/release2'
-# classes = {'babyfood': ['meltacchin', 'melpollo'],
-#            'babymilk': ['humana1', 'humana2']}
+classes = {'babyfood': ['meltacchin', 'melpollo'],
+           'babymilk': ['humana1', 'humana2']}
 
-classes = {'babymilk': ['humana1', 'humana2']}
+# classes = {'babymilk': ['humana1', 'humana2']}
 
 all_subclasses = sum([subcl for _, subcl in classes.items()], [])
 
@@ -59,7 +59,7 @@ def proc_results_f(results_array):
 proc_results = proc_results_f(results)
 
 
-def plot_results(some_results, title, threshold, labels=True, baseline=True, color=None, legend=True, starting_from=10):
+def plot_results(some_results, title, threshold, labels=True, baseline=True, color=None, legend=True, starting_from=10, fix_y_dims=(0.3, 1)):
     X = np.linspace(0, 1, 100)
     Y = np.zeros_like(X)
     sigma = 0.05
@@ -75,6 +75,8 @@ def plot_results(some_results, title, threshold, labels=True, baseline=True, col
 
     plt.ylabel('correct classification probability')
     plt.xlabel('for view ambiguity ranks less than X')
+    if fix_y_dims is not None:
+        plt.ylim(fix_y_dims)
     plt.title('%s of dataset\ntaking %s of ambiguity ranks (assuming two different models)' % (threshold, title))
     lbl = 'success probability for rank less than (x)'
     if starting_from > 0:
@@ -98,7 +100,7 @@ def plot_results(some_results, title, threshold, labels=True, baseline=True, col
 lessthan = [0.25, 0.4, 0.5, 0.7]
 
 def plot_results_from_array(results_array, func_name, title, threshold, labels=None, color=None, legend=True,
-                            starting_from=10, half_line=False):
+                            starting_from=10, half_line=False, fix_y_dims=(0.3, 1)):
     if labels is None:
         labels = [None] * len(results_array)
     min_r = 1
@@ -135,11 +137,11 @@ with open(results_file, 'rb') as f:
     results_dict = pickle.load(f)
 
 # plot_results(proc_results_f(results_dict[2][0.4])['average'], 0.5, 'aga')
-figfolder = '/home/safoex/Documents/data/aae/draw/milk'
-figpath = figfolder + "/inference2_0%d.png"
+# figfolder = '/home/safoex/Documents/data/aae/draw/24'
+# figpath = figfolder + "/inference2_0%d.png"
 
-# figfolder = '/home/safoex/Documents/docs/writings/ambiguousobjectspaper/images/plots/22'
-# figpath = figfolder + '/inference2_0%d.pdf'
+figfolder = '/home/safoex/Documents/docs/writings/ambiguousobjectspaper/images/plots/24'
+figpath = figfolder + '/inference2_0%d.pdf'
 if not os.path.exists(figfolder):
     os.mkdir(figfolder)
 
@@ -150,7 +152,7 @@ if not os.path.exists(figfolder):
 # plt.show()
 
 lessthan_performance = {k:list() for k in lessthan}
-last_threshold_ = 0.7
+last_threshold_ = 1
 last_threshold = int(last_threshold_ * 20)
 for t in range(3, last_threshold):
     threshold = t / 20.0
@@ -173,4 +175,4 @@ plt.title('Performance of best among all epochs classifiers \ntested on $rank(R^
 
 plt.savefig(figfolder + '/sumup.pdf')
 plt.savefig(figfolder + '/sumup.png')
-plt.show()
+# plt.show()

mitmpose/routines/test/inference/classify.py
@@ -303,7 +303,7 @@ class InferenceClassifier:
             n_subclasses = len(self.hcl.classes[gcl])
             orientation_hypothesis = list()
             for i, lcl in enumerate(self.hcl.classes[gcl]):
-                tops, idcs = torch.topk(self.cdbks[lcl].cos_sim(aae.encoder.forward(t_input)), 2)
+                tops, idcs = torch.topk(self.cdbks[lcl].cos_sim(aae.encoder.forward(t_input)), 1)
                 max_score = 0
                 for idx in idcs:
                     rot = self.cdbks[lcl].grider.grid[idx]

mitmpose/routines/test/inference/next_pose.py
@@ -6,11 +6,11 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 import torch
 import os
-
+import shutil
 
 def correct_camera_rot(camera_rot):
     camera_euler = Rotation.from_matrix(camera_rot).as_euler('xyz')
-    # camera_euler[0] -= np.pi
+    camera_euler[0] -= np.pi
     return Rotation.from_euler('xyz', camera_euler).as_matrix()
 
 
@@ -79,8 +79,12 @@ class TrajectoryGenerator:
                 self.rots.append(rot_matrix)
 
 
+# test_folder + 'img_%d_re_%d.png' % (idx + 1, i_)
+from torchvision import  transforms as T
+
+
 class NextPoseProvider:
-    def __init__(self, workdir, ds, possible_orientations, device, classes, aae_params=(128, 256, (128, 256, 256, 512)), grider_labeled_size=300):
+    def __init__(self, workdir, ds, possible_orientations, device, classes, aae_params=(128, 256, (128, 256, 256, 512)), grider_labeled_size=300, fr=None):
         self.workdir = workdir
         self.ds = ds
         self.aae_params = aae_params
@@ -91,6 +95,7 @@ class NextPoseProvider:
         self.icl = InferenceClassifier(self.hcl, device, self.cache)
         self.possible_orientations = possible_orientations
         self.classes = classes
+        self.fr = fr
 
     def load_or_render_codebooks(self, cdbk_grider=Grid(4000, 40)):
         for gcl, subclasses in self.classes.items():
@@ -103,15 +108,28 @@ class NextPoseProvider:
                 else:
                     self.icl.cdbks[lcl].save(path_to_cdbk)
 
+    def print_rendered_pair(self, rots, save_path, assumed_class):
+        for i_, rot in enumerate(rots):
+            match_img, mask = self.icl.cdbks[self.classes[assumed_class][i_]]._ds.objren.render_and_crop(rot)
+            T.ToPILImage()(torch.from_numpy(np.moveaxis(match_img, 2, 0) / 255.0)).save(save_path % i_)
+
+    def print_rendered(self, rot, save_path, assumed_class, lcln):
+        match_img, mask = self.icl.cdbks[self.classes[assumed_class][lcln]]._ds.objren.render_and_crop(rot)
+        T.ToPILImage()(torch.from_numpy(np.moveaxis(match_img, 2, 0) / 255.0)).save(save_path)
+
     def load_everything(self, extra_folder_local, extra_folder_global=""):
         self.hcl.manual_set_classes(self.classes)
         self.hcl.load_everything(extra_folder_global, extra_folder_local)
 
-    def classify(self, image_path, robot_orientation, assume_global_class=None, ambiguity_threshold=0.4, next_random=True):
+    def classify(self, image_path, robot_orientation, assume_global_class=None, ambiguity_threshold=0.4, next_random=True, first_i=None):
         # orientation as matrix
-        result, orientation_hypothesis = self.icl.classify2(image_path, assume_global_class=assume_global_class, cache=self.cache)
+        res = self.icl.classify2(image_path, assume_global_class=assume_global_class, cache=self.cache)
+        if res is None:
+            return None, None
+        result, orientation_hypothesis = res
+
         if sum((result[2], result[3]))/2 > ambiguity_threshold:
-            return result, self.next_pose_(orientation_hypothesis, robot_orientation, assume_global_class, next_random)
+            return result, self.next_pose_(orientation_hypothesis, robot_orientation, assume_global_class, next_random, first_i=first_i)
         else:
             return result, None
 
@@ -120,30 +138,125 @@ class NextPoseProvider:
         rot = self.possible_orientations[idx]
         return (1, rot), idx
 
-    def next_pose_(self, orientation_hypothesis, robot_orientation, global_class, next_random):
+    def next_pose_(self, orientation_hypothesis, robot_orientation, global_class, next_random, first_i):
         if next_random:
             return self.next_pose_random_()
         n_subclasses = self.hcl.labelers[global_class]._sorted.shape[2]
         min_ambig_pose = (1, None)
         min_i = None
-        for k, camera_pose in enumerate(self.possible_orientations):
+
+        lcls = self.classes[global_class]
+        best_expected_poses = [None, None]
+
+        testclass = 'melpollo'
+        panda_dir = "/home/safoex/Documents/data/aae/panda_data_10_02_2021/%s/%s_0/rad_0.35" % (testclass, testclass)
+        nmoves_dir = panda_dir + '/next_moves/'
+        if not os.path.exists(nmoves_dir):
+            os.mkdir(nmoves_dir)
+
+        for k, new_camera_pose in enumerate(self.possible_orientations):
             if k % 5 != 0:
                 continue
             max_score = 0
+            possible_object_poses = [None, None]
             for identity, object_orientation in orientation_hypothesis:
-                # corrected_object_pose = object_frame(robot_orientation, object_orientation)
-                # possible_object_pose = object_frame(camera_pose, corrected_object_pose)
-                possible_object_pose = camera_frame(camera_pose, object_orientation)
+                # corrected_object_pose = camera_frame(robot_orientation, object_orientation)
+                # possible_object_pose = object_frame(new_camera_pose, corrected_object_pose)
+
+                # camera_pose_ = correct_camera_rot(new_camera_pose)
+                # robot_orientation_ = correct_camera_rot(robot_orientation)
+                # possible_object_pose = camera_pose_.T.dot(robot_orientation_.dot(object_orientation))
+                # possible_object_pose = new_camera_pose.dot(robot_orientation.T.dot(object_orientation))
+                delta_robot = robot_orientation.T.dot(new_camera_pose)
+                # print('delta_robot:')
+                # print(delta_robot)
+                # print()
+                # print('-----------')
+                # print()
+                Rx = Rotation.from_euler('xyz', [np.pi, 0, 0]).as_matrix()
+                Rz = Rotation.from_euler('xyz', [0, 0, -np.pi/2]).as_matrix()
+                Rextra = Rx.dot(Rz)
+                possible_object_pose = object_orientation.T.dot(Rextra).dot(delta_robot).dot(Rextra.T).T
+                # possible_object_pose = object_orientation.T.dot(delta_robot).T
+                # possible_object_pose = np.eye(3).dot(object_orientation.T).T
+                possible_object_poses[identity] = possible_object_pose
+                # print(identity)
+
+                # self.print_rendered(possible_object_pose, nmoves_dir + '/im_' + str(k + 1) + '_x%d.png' % identity,
+                #                          global_class, identity)
+
+                # if identity == 0:
+                #     self.print_rendered(possible_object_pose, nmoves_dir + '/im_' + str(k + 1) + '_nex.png',
+                #                         global_class, identity)
+                #     self.print_rendered(object_orientation, nmoves_dir + '/im_' + str(k + 1) + '_ini.png' ,
+                #                         global_class, identity)
+
                 close_idcs = self.icl.get_closest_without_inplane(possible_object_pose,
                                                                   self.hcl.labelers[global_class].grider.grid, 1)
+
                 scores = torch.cat(
                     tuple(self.hcl.labelers[global_class]._sorted[close_idcs, identity, j] for j in range(n_subclasses) if identity != j))
                 # print(scores)
                 if torch.max(scores) > max_score:
                     max_score = torch.max(scores)
 
+            # shutil.copy(self.fr.image_paths[first_i], nmoves_dir + 'im_%d_in.png' % (k + 1))
+            # shutil.copy(self.fr.image_paths[k], nmoves_dir + 'im_%d_ne.png' % (k + 1))
+            #
+            # for _id in range(2):
+            #     self.print_rendered(self.fr.traj.rots[k], nmoves_dir + '/im_' + str(k + 1) + '_x%d.png' % (2 + _id),
+            #                     global_class, _id)
+
+            # res2 = self.icl.classify2(self.fr.image_paths[k], assume_global_class=global_class, cache=self.cache)
+            # if res2 is not None:
+            #     result2, orientation_hypothesis2 = res2
+            #     # print(orientation_hypothesis2)
+            #     obj_r_list = [None, None]
+            #     for _i, _or in orientation_hypothesis:
+            #         obj_r_list[_i] = _or
+            #
+            #     obj_r_list2 = [None, None]
+            #     for _i, _or in orientation_hypothesis2:
+            #         obj_r_list2[_i] = _or
+            #
+            #     obj_delta = [or2.T.dot(or1) for or1, or2 in zip(obj_r_list, obj_r_list2)]
+            #     camera_delta = new_camera_pose.T.dot(robot_orientation)
+            #     print('*****************************************')
+            #     print()
+            #     def print_rot(rot):
+            #         print(["%.3f"%e for e in Rotation.from_matrix(rot).as_euler('xyz')])
+            #
+            #     for obj_i, obj_d in enumerate(obj_delta):
+            #         print("obj_delta%d: " % obj_i)
+            #         print(print_rot(obj_d))
+            #         print()
+            #     print('camera_delta: ')
+            #     print(print_rot(camera_delta))
+            #     print()
+            #     print()
+
+            # self.print_rendered_pair(possible_object_poses, nmoves_dir + '/im_' + str(k+1) + '_x%d.png', global_class)
+
             if max_score < min_ambig_pose[0]:
-                min_ambig_pose = (max_score, camera_pose)
+                min_ambig_pose = (max_score, new_camera_pose)
                 min_i = k
+                best_expected_poses = possible_object_poses
+
+        # print("H: ")
+        # print(robot_orientation)
+        #
+        # print("H_next: ")
+        # print(min_ambig_pose[1])
+        #
+        # print("H0: ")
+        # for _, object_pose in orientation_hypothesis:
+        #     print(object_pose)
+        #
+        # print()
+        # print('--------------------------')
+        # print()
+
+
         # print("best next image is ", min_i, self.possible_orientations[min_i])
-        return min_ambig_pose, min_i
+
+        return min_ambig_pose, min_i, best_expected_poses

mitmpose/routines/test/inference/next_pose_check.py
@@ -1,5 +1,8 @@
 from mitmpose.routines.test.inference.next_pose import *
 from mitmpose.model.classification.dataset_hierarchical import *
+import shutil
+from torchvision import transforms as T
+
 
 class FakeResponse:
     def __init__(self, recorded_data_dir):
@@ -21,7 +24,7 @@ class FakeResponse:
         return self.get_i(idx)
 
     def get_idx_of(self, rot):
-        return np.argmin(np.linalg.norm(np.array(self.traj.rots) - rot, axis=(1,2)))
+        return np.argmin(np.linalg.norm(np.array(self.traj.rots) - rot, axis=(1, 2)))
 
     def get_by_rot(self, rot):
         return self.get_i(self.get_idx_of(rot))
@@ -29,12 +32,10 @@ class FakeResponse:
 
 workdir = "/home/safoex/Documents/data/aae/release3"
 
-
 assumed_class = 'babyfood'
 testclass = 'melpollo'
 
-
-panda_dir = "/home/safoex/Documents/data/aae/panda_data_10_02_2021/%s/%s_2/rad_0.35" % (testclass, testclass)
+panda_dir = "/home/safoex/Documents/data/aae/panda_data_10_02_2021/%s/%s_0/rad_0.35" % (testclass, testclass)
 
 fr = FakeResponse(panda_dir)
 
@@ -53,11 +54,9 @@ ds = HierarchicalManyObjectsDataset(grider, models, res=236,
                                                                       '/home/safoex/Documents/data/VOCtrainval_11-May-2012',
                                                                       add_patches=False, size=(236, 236)))
 
-nipple = NextPoseProvider(workdir, ds, fr.traj.rots, device, classes)
+nipple = NextPoseProvider(workdir, ds, fr.traj.rots, device, classes, fr=fr)
 nipple.load_everything("", "")
 nipple.load_or_render_codebooks()
-
-
 
 checkdir = panda_dir + '/check/'
 if not os.path.exists(checkdir):
@@ -69,7 +68,6 @@ if not os.path.exists(checkdir):
 #                         checkdir + 'img_%d_rec.png' % i, checkdir + 'img_%d' % i + '_z_%d.png', assume_global_class=assumed_class)
 
 
-
 succeded = 0
 improved = 0
 worsen = 0
@@ -77,25 +75,47 @@ total = 0
 incorrect = 0
 
 ambiguity_threshold = 0.3
-jump_limit = 6
-for _ in range(100):
+jump_limit = 1
+test_folder = panda_dir + '/next_corrected_camera_frame/'
+if not os.path.exists(test_folder):
+    os.mkdir(test_folder)
+
+axis = ['x', 'y', 'z']
+
+# pose = special_ortho_group.rvs(3)
+# match_img, mask = nipple.icl.cdbks[classes[assumed_class][0]]._ds.objren.render_and_crop(pose)
+# T.ToPILImage()(torch.from_numpy(np.moveaxis(match_img, 2, 0) / 255.0)).save(test_folder + "_ident.png")
+#
+# for i_axis, ax in enumerate(axis):
+#     eulers = [0, 0, 0]
+#     eulers[i_axis] = np.pi/2
+#     delta_pose = Rotation.from_euler('xyz', eulers).as_matrix()
+#     pose_end = pose.T.dot(delta_pose).T
+#     match_img, mask = nipple.icl.cdbks[classes[assumed_class][0]]._ds.objren.render_and_crop(pose_end)
+#     T.ToPILImage()(torch.from_numpy(np.moveaxis(match_img, 2, 0) / 255.0)).save(test_folder + "_ident_%s.png" % ax)
+
+all_best_poses = []
+for idx in range(100):
     ambiguity = 1
     tabs = ''
-    next_rot = fr.traj.rots[fr.gen_random_idx()]
+    next_rot = fr.traj.rots[idx]
     initial_class = None
     final_class = None
+    j = 0
     try:
-        j = 0
         while ambiguity > ambiguity_threshold and j < jump_limit:
             image_path, rot = fr.get_by_rot(next_rot)
-            result = nipple.classify(image_path, rot, assumed_class, ambiguity_threshold, next_random=False)
+            result = nipple.classify(image_path, rot, assumed_class, ambiguity_threshold, next_random=False, first_i=idx)
             print(tabs, result[0], result[1][1] if result[1] is not None else -1)
             if initial_class is None:
                 initial_class = result[0][4]
             if result[1] is not None:
-                (expected_ambiguity, next_rot), next_i = result[1]
+                (expected_ambiguity, next_rot), next_i, best_poses = result[1]
                 print(tabs, expected_ambiguity)
                 tabs += '\t'
+                shutil.copy(fr.image_paths[idx], test_folder + 'img_%d_in.png' % (idx + 1))
+                shutil.copy(fr.image_paths[next_i], test_folder + 'img_%d_ne.png' % (idx + 1))
+                all_best_poses.append((idx, best_poses))
                 j += 1
             else:
                 ambiguity = 0
@@ -104,7 +124,6 @@ for _ in range(100):
                 final_class = result[0][4]
     except:
         pass
-
     total += 1
 
     if final_class == testclass:
@@ -125,3 +144,10 @@ print('succeded : %d' % succeded)
 print('incorrect : %d' % incorrect)
 print('improved : %d' % improved)
 print('worsen : %d' % worsen)
+#
+#
+
+for idx, best_poses in all_best_poses:
+    for i_, pose in enumerate(best_poses):
+        match_img, mask = nipple.icl.cdbks[classes[assumed_class][i_]]._ds.objren.render_and_crop(pose)
+        T.ToPILImage()(torch.from_numpy(np.moveaxis(match_img, 2, 0) / 255.0)).save(test_folder + 'img_%d_re_%d.png' % (idx + 1, i_))